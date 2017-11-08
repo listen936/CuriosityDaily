d323bc686c5e7b355f45789129e2498889a4516f
czztracy <879742352@qq.com>
2017-11-08 20:58:30 +0800

    article
    123

--- components/article/article.vue
+++ components/article/article.vue
@@ -1,3 +1,28 @@
 <template>
-  <div>article</div>
+  <div class="title">{{name}}</div>
 </template>
+
+
+<script>
+	export default() {
+		data() {
+			return {
+				name: "详情页"
+			}
+		}
+	}
+</script>
+
+
+<style lang="scss" scoped>
+	$size: 40px;
+	.title{
+		height: $size;
+		line-height: $size;
+		width: 100%;
+		background: black;
+		color: 20px;
+		font-size: 800;
+		text-align: center;
+	}
+</style>
