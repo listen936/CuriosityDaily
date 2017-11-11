66890c1ec10bd43a7e31e35f25b8311a29f725a8
czztracy <879742352@qq.com>
2017-11-11 16:43:33 +0800

    article
    点击大图

--- components/article/article.vue
+++ components/article/article.vue
@@ -2,6 +2,7 @@
 	<div class="mobile articles show box">
 		<xheader></xheader>
 		<xmain></xmain>
+		<xbg></xbg>
 		<router-view></router-view>
 	</div>
 </template>
@@ -9,10 +10,12 @@
 <script>
 	import xheader from './header.vue'
 	import xmain from './main.vue'
+	import xbg from './bg.vue'
 	export default {
 		components: {
 			xheader,
-			xmain
+			xmain,
+			xbg
 		}
 	}
 </script>
---
+++ components/article/bg.vue
@@ -0,0 +1,41 @@
+<template>
+	<div id="Weui" :style="{'display':bool? 'block': 'none'}">
+		<div class="page gallery js_show">
+    <div class="page__hd">
+        <h1 class="page__title">Gallery</h1>
+        <p class="page__desc">画廊，可实现上传图片的展示或幻灯片播放</p>
+    </div>
+    <div class="weui-gallery" style="display: block">
+        <span class="weui-gallery__img" style="background-image: url(../components/article/images/iPhone.jpg);"></span>
+        <div class="weui-gallery__opr">
+            <a href="javascript:" class="weui-gallery__del">
+                <i @click="Show"  class="weui-icon-delete weui-icon_gallery-delete"></i>
+            </a>
+        </div>
+    </div>
+</div>
+	</div>
+</template>
+
+<script>
+	import $ from "jquery";
+	export default {
+		data() {
+			return {
+				bool: false
+			}
+		},
+		methods:{
+			Show: function(){
+				this.bool = false;
+			}
+		}
+	}
+</script>
+
+<style scoped>
+	#Weui{
+		position: absolute;
+		top:0;
+	}
+</style>
